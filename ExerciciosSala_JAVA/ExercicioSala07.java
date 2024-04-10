13e7bb217fbf47fb081531616b1f9fce04a21115
CarlosHelano <carloshelano@gmail.com>
2024-04-10 15:38:03 -0300

    Add files via upload

---
+++ ExerciciosSala_JAVA/ExercicioSala07.java
@@ -0,0 +1,24 @@
+public class ExercicioSala07 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Exibindo os numeros de 1 a 100");
+        System.out.println("Repeticao com for");
+        
+        for (int i = 1; i <= 100; i++){
+            System.out.println("i : " + i);
+        }
+       
+        System.out.println("Exibindo os numeros de 1 a 100");
+        System.out.println("Repeticao com while");
+        
+        int cont = 1;
+        while (cont <= 100)
+        {
+           System.out.println("cont : " + cont);
+           cont++;
+        }
+        
+    }
+    
+}
